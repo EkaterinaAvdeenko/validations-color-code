8afcf6b684a0f9e761a12d1ba9fad63b16b69bd0
Avdeenko Ekaterina <avdeenqoeka@gmail.com>
2023-12-17 21:20:39 +0100

    validations-color-code_AvdeenkoEkaterina

--- src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
+++ src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
@@ -4,19 +4,17 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ColorCodeValidation {
-    public static boolean validateColorCode(String color) {
-        String regex = "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$";
-        Pattern p = Pattern.compile(regex);
-        // Если строка пустая, то вернуть ноль
-        // вернуть false
-        if (color == null) {
-            return false;
-        }
-        // Pattern class содержит matcher() метод
-        Matcher m = p.matcher(color);
-        return m.matches();
 
+  public static boolean validateColorCode(String color) {
+    String regex = "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$";
+    Pattern p = Pattern.compile(regex);
+    if (color == null) {
+      return false;
     }
+    Matcher m = p.matcher(color);
+    return m.matches();
+
+  }
 }
 
 
